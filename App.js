248869b446a5ea8caf2db1a7f5a438964fa1ea04
Grant <gclane1@ilstu.edu>
2023-07-09 16:31:58 -0500

    Uploading an image

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
+import UploadScreen from "./app/screens/UploadScreen";
 
 export default function App() {
   return (
