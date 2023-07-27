bcbf7dc464f08ce7372e5f695a0554e95894832e
grant.lane <grant.lane@lfnd.org>
2023-07-27 12:07:32 -0500

    Inspecting AsyncStorage

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
-import AppButton from "./app/components/AppButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const netInfo = useNetInfo();
-
-  return (
-    <AppButton
-      disabled={netInfo.isInternetReachable}
-      title="Press"
-      onPress={() => console.log("Laugh")}
-    />
-  );
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  demo();
+  return null;
 }
