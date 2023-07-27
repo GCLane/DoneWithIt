342f8c5510f5b22a21753079c30f07ca33223aec
grant.lane <grant.lane@lfnd.org>
2023-07-27 13:40:50 -0500

    Caching Successful Request Data

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
-  const demo = async () => {
-    try {
-      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
-      const value = await AsyncStorage.getItem("person");
-      const person = JSON.parse(value);
-      console.log(person);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  demo();
-  return null;
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
