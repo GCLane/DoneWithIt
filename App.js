8110728b19309109e486fb417053261ddda98580
grant.lane <grant.lane@lfnd.org>
2023-07-28 09:59:48 -0500

    Extracting the User from the Authtoken

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import AppNavigator from "./app/navigation/AppNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <>
       <OfflineNotice />
       <NavigationContainer>
-        <AppNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
